48a55546b25e46fa39b11cb3b12387237931b9d0
akhilesh <akhileshgowda7@gmail.com>
2022-05-10 00:11:22 -0400

    may 9 end of day commit

--- Project-1/src/app/recipes/recipe.model.ts
+++ Project-1/src/app/recipes/recipe.model.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from "../shared/ingredient.model";
 
 
 
@@ -5,12 +6,14 @@ export class Recipe{
    public name:string;
    public description:string;
    public imagePath:string;
+   public ingredients : Ingredient[];
 
-   constructor(name:string, desc:string, imagePath:string)
+   constructor(name:string, desc:string, imagePath:string , ingredients:Ingredient[])
    {
        this.name = name
        this.description = desc
-       this.imagePath = imagePath
+       this.imagePath = imagePath;
+       this.ingredients = ingredients;
 
    }
 }
