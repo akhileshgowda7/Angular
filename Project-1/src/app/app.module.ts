49e54f646b781c108bf4720accb923b40b9b6142
akhilesh <akhileshgowda7@gmail.com>
2022-05-11 21:07:34 -0400

    Added Routing Feature

--- Project-1/src/app/app.module.ts
+++ Project-1/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,6 +11,9 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdoenDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { FormsModule } from '@angular/forms';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 @NgModule({
@@ -24,10 +26,13 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdoenDirective
+    DropdoenDirective,
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService],
---
+++ Project-1/src/app/recipes/recipe.service.ts
@@ -0,0 +1,33 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService{
+    recipeSelected = new EventEmitter<Recipe>()
+    private recipes:Recipe[] =[
+        new Recipe('Tasty recipe','Just a test image','https://i.pinimg.com/564x/34/11/91/341191c8a908e5533c3257bc973b71c0.jpg',[
+            new Ingredient('Meat',1), new Ingredient('Fries',5)
+        ]),
+        new Recipe('Burger','Just a test image','https://i.pinimg.com/564x/83/fe/4f/83fe4fb43c158ba1af17ba351c06fe22.jpg',[
+            new Ingredient('Buns',2), new Ingredient('Patties',1)
+        ])
+    
+      ];
+      constructor(private slService:ShoppingListService){
+
+      }
+      getRecipes(){
+          return this.recipes.slice(); //returs a new array as a copy
+      }
+
+    getRecipe(index:number){
+        return this.recipes[index];
+    }
+
+
+      addIngredientsToShoppingList(ingredients:Ingredient[]){
+        this.slService.addIngredients(ingredients);
+      }
+}
