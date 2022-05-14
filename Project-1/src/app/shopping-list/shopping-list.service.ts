b68d8fa347823db3fa56a53f4b87fff790a9e958
akhilesh <akhileshgowda7@gmail.com>
2022-05-14 15:11:08 -0400

    Adding Delete Button Functionality

--- Project-1/src/app/shopping-list/shopping-list.service.ts
+++ Project-1/src/app/shopping-list/shopping-list.service.ts
@@ -27,5 +27,9 @@ export class ShoppingListService{
         this.ingredients[index] = newIngridient;
         this.ingredientsChanged.next(this.ingredients.slice());
     }
+    deleteIngredient(index:number){
+       this.ingredients.splice(index,1);
+       this.ingredientsChanged.next(this.ingredients.slice());
+    }
 
 }
