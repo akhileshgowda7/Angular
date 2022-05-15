c9283b5f76ff5ca10b4647199793e72a3a0ecaa8
akhilesh <akhileshgowda7@gmail.com>
2022-05-15 10:57:17 -0400

    Incorporating changes to recipe detail

--- Project-1/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Project-1/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -28,5 +28,9 @@ export class RecipeDetailComponent implements OnInit {
   onEditRecipe(){
    this.router.navigate(['edit'],{relativeTo: this.route});
   }
+  ondeleteRecipe(){
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['recipes']);
+    }
 
 }
