ce33ca8c51c39cb2faccb771121131df1afbeec4
akhilesh <akhileshgowda7@gmail.com>
2022-05-17 17:20:19 -0400

    adding the click listener to fetch and save data

--- Project-1/src/app/header/header.component.ts
+++ Project-1/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 
 
@@ -8,5 +9,12 @@ import { Component } from "@angular/core";
 
 })
 export class HeaderComponent{
- 
+   constructor(private dataStorageService:DataStorageService){}
+
+   onSaveData(){
+       this.dataStorageService.storeRecipes();
+   }
+   onFetchData(){
+       this.dataStorageService.fetchRecipes();
+   }
 }
