e0f15e2e450503bc40773018b0778bdc0e0b7386
akhilesh <akhileshgowda7@gmail.com>
2022-05-18 17:19:50 -0400

    resolving data before loading

--- Project-1/src/app/recipes/recipe.service.ts
+++ Project-1/src/app/recipes/recipe.service.ts
@@ -9,15 +9,16 @@ import { Recipe } from "./recipe.model";
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes:Recipe[] =[
-        new Recipe('Tasty recipe','Just a test image','https://i.pinimg.com/564x/34/11/91/341191c8a908e5533c3257bc973b71c0.jpg',[
-            new Ingredient('Meat',1), new Ingredient('Fries',5)
-        ]),
-        new Recipe('Burger','Just a test image','https://i.pinimg.com/564x/83/fe/4f/83fe4fb43c158ba1af17ba351c06fe22.jpg',[
-            new Ingredient('Buns',2), new Ingredient('Patties',1)
-        ])
+    // // private recipes:Recipe[] =[
+    // //     new Recipe('Tasty recipe','Just a test image','https://i.pinimg.com/564x/34/11/91/341191c8a908e5533c3257bc973b71c0.jpg',[
+    // //         new Ingredient('Meat',1), new Ingredient('Fries',5)
+    //     ]),
+    //     new Recipe('Burger','Just a test image','https://i.pinimg.com/564x/83/fe/4f/83fe4fb43c158ba1af17ba351c06fe22.jpg',[
+    //         new Ingredient('Buns',2), new Ingredient('Patties',1)
+    //     ])
     
-      ];
+    //   ];
+    private recipes:Recipe[]=[];
       constructor(private slService:ShoppingListService){}
     setRecipes(recipes:Recipe[]){
       this.recipes =recipes;
